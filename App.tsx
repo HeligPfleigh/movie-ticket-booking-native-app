445ee288a1cc5454a5a84d6950a59b60fbcfc4de
goldenhands92 <trantuan1892@gmail.com>
2020-07-25 21:51:20 +0700

    refactor: upgrade to RN version 0.63

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "react-native-gesture-handler";
+
+import Main from "./src/app";
+
+export default class App extends Component<{}> {
+  render() {
+    return <Main />;
+  }
+}
